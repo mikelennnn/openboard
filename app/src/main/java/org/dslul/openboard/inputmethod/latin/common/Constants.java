678d06d8b4b31eb71dd2cf7163f1ea313eb6369a
mikelennnn <mikelbarrientos28@gmail.com>
2026-09-12 12:56:40 -0600

    Update Constants.java

--- app/src/main/java/org/dslul/openboard/inputmethod/latin/common/Constants.java
+++ app/src/main/java/org/dslul/openboard/inputmethod/latin/common/Constants.java
@@ -255,6 +255,8 @@ public final class Constants {
     public static final int CODE_AI_ASSIST = -21;
     public static final int CODE_CORRECT_TEXT = -22;
     public static final int CODE_RUDE_TEXT = -23;
+    public static final int CODE_TRANSLATE_TEXT = -24;
+    public static final int CODE_FORMAL_TEXT = -25;
 
     public static boolean isLetterCode(final int code) {
         return code >= CODE_SPACE;
@@ -282,6 +284,8 @@ public final class Constants {
         case CODE_AI_ASSIST: return "aiAssist";
         case CODE_CORRECT_TEXT: return "correctText";
         case CODE_RUDE_TEXT: return "rudeText";
+        case CODE_TRANSLATE_TEXT: return "translateText";
+        case CODE_FORMAL_TEXT: return "formalText";
         case CODE_TAB: return "tab";
         case CODE_ENTER: return "enter";
         case CODE_SPACE: return "space";
