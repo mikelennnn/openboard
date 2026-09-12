e66a92b08bebd25c635b6babb88d95e952287276
mikelennnn <mikelbarrientos28@gmail.com>
2026-09-11 21:30:43 -0600

    Update Constants.java

--- app/src/main/java/org/dslul/openboard/inputmethod/latin/common/Constants.java
+++ app/src/main/java/org/dslul/openboard/inputmethod/latin/common/Constants.java
@@ -252,6 +252,7 @@ public final class Constants {
     public static final int CODE_SWITCH_ONE_HANDED_MODE = -19;
     // Code value representing the code is not specified.
     public static final int CODE_UNSPECIFIED = -20;
+    public static final int CODE_AI_ASSIST = -21;
 
     public static boolean isLetterCode(final int code) {
         return code >= CODE_SPACE;
@@ -276,6 +277,7 @@ public final class Constants {
         case CODE_ALPHA_FROM_EMOJI: return "alpha";
         case CODE_ALPHA_FROM_CLIPBOARD: return "alpha";
         case CODE_UNSPECIFIED: return "unspec";
+        case CODE_AI_ASSIST: return "aiAssist";
         case CODE_TAB: return "tab";
         case CODE_ENTER: return "enter";
         case CODE_SPACE: return "space";
